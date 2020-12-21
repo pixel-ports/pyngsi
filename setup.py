82b6f36e502117334bace09e05054a11b73834cf
fbattello <fabien.battello@orange.com>
2020-12-21 19:11:09 +0100

    Fixed bad classifier in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9"
     ],
     packages=setuptools.find_packages(),
